150232012c654619350c43087d3997c9c44b8c03
DarkBlue2021 <weihai.he@hotmail.com>
2021-11-14 15:46:07 +1100

    intial

--- dapp.js
+++ dapp.js
@@ -1,12 +1,37 @@
 console.log("My Home Energy!")
 
-const ssAddress =  '0x01c1f08D2Fb49678A948286a8075fa48ea662bab'
+//const ssAddress =  '0x01c1f08D2Fb49678A948286a8075fa48ea662bab' // Storage
+const ssAddress = '0xff43b5B1C46BD39B3d60E79CA0fc67369f52F7b6'    // Energy Bill
 
 const ssABI =
 [
 	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_num",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
 		"inputs": [],
-		"name": "retrieve",
+		"name": "billData",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getBillData",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -21,11 +46,11 @@ const ssABI =
 		"inputs": [
 			{
 				"internalType": "uint256",
-				"name": "num",
+				"name": "x",
 				"type": "uint256"
 			}
 		],
-		"name": "store",
+		"name": "setBillData",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
@@ -55,24 +80,40 @@ mmEnable.onclick = async() => {
 }
 
 const ssSubmit = document.getElementById('ss-input-button');
-
 ssSubmit.onclick = async () => {
 
 	let mmTxStatus = document.getElementById('mm-transaction-status')
-	mmTxStatus.innerHTML = 'Processing transaction ... '
-
+	
 	const ssValue = document.getElementById('ss-input-box').value;
 	console.log(ssValue)
 
 	var web3 = new Web3(window.ethereum)
 
-	const simpleStorage = new web3.eth.Contract (ssABI, ssAddress)
+	const energyBill = new web3.eth.Contract (ssABI, ssAddress)
 
-	simpleStorage.setProvider(window.ethereum)
+	energyBill.setProvider(window.ethereum)
 
-	let txStatus = await simpleStorage.methods.store(ssValue).send({from: ethereum.selectedAddress})
+	try{
 
-	mmTxStatus.innerHTML = 'Transaction processed ! Status: ' + txStatus
+		if (isNaN(ssValue) || (ssValue.length==0)){
+			mmTxStatus.innerHTML = '<span style="color:red">Input must not be empty, and must be numeric value! </span>'
+		} else{
+			mmTxStatus.innerHTML = '<span style="color:blue"> Processing transaction ... </span>'	
+
+			await energyBill.methods.setBillData(ssValue).send({from: ethereum.selectedAddress})
+			.on('confirmation',  function(confirmationNumber, receipt){
+				//console.log(confirmationNumber)
+				mmTxStatus.innerHTML = '<span style="color:green"> Transaction processed SUCESSFULLY! TxHash: ' + receipt["transactionHash"]  + "</span>"
+			})
+			.on('error',  function(error, receipt){
+				//console.log(error)
+				mmTxStatus.innerHTML = '<span style="color:red">Transaction processed FAILED ! Error: ' + error["message"] + "</span>"
+			});
+		}
+	} catch {
+		//mmTxStatus.innerHTML = '<span style="color:red">Unexpected error ! </span>'
+	}
 }
+
 
 
